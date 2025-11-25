5568085f3517ff32d4675c4b9b138a4b0cb38747
0xunlin <priyantarjonak9@gmail.com>
2025-11-25 16:19:40 +0000

    Save progress before installing Chainlink

--- src/SmartProfitTaker.sol
+++ src/SmartProfitTaker.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
-import "@chainlink/contracts/src/v0.8/automation/AutomationCompatible.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "lib/openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "lib/openzeppelin-contracts/contracts/utils/ReentrancyGuard.sol";
+import "lib/openzeppelin-contracts/contracts/src/v0.8/automation/AutomationCompatible.sol";
+import "lib/openzeppelin-contracts/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 // Interfaces for Aave and Uniswap
 interface IPool {
